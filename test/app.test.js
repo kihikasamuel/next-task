baad87efadbc342d71d4858fee845ecc09e8b71b
Summitm <summitros@gmail.com>
2021-11-13 20:14:48 +0300

    .

--- test/app.test.js
+++ test/app.test.js
@@ -2,22 +2,24 @@ const app = require('../api/index');
 const request = require('supertest');
 
 describe("GET /api/tasks/all", () => {
-    test("It should respond with status code 200", async () => {
+    test("It should respond with object keys", async () => {
         const response = await request(app).get('/tasks/all');
-        expect(response.statusCode).toBe(200);
-    });
-
-    test("It should return status code 200", async () => {
-        const response = await request(app).get('/tasks/:1');
-        
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({'id': 1})
+            ])
+        );
+        expect(response.body[0]).toHaveProperty('label');
+        expect(response.body[1]).toHaveProperty('id');
         expect(response.statusCode).toBe(200);
     });
 });
 
 describe("Get /api/tasks/:id", () => {
     test("It should return status code 200", async () => {
-        const response = await request(app).get('/tasks/:1');
-        
+        let id = 3;
+        const response = await request(app).get('/tasks/:id');
+        // expect(response.body[0]).toHaveProperty('id');
         expect(response.statusCode).toBe(200);
     });
 });
