21475dfc187413c5632bd98878b96ceeafc3e7c5
Summitm <summitros@gmail.com>
2022-04-10 14:15:51 +0300

    added target to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,9 @@ export default {
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
 
+    // target
+    target: 'server',
+
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/tailwindcss
