69e63a7581c0696ad8822215212fce5a71df3e3e
Summitm <summitros@gmail.com>
2022-04-10 14:52:52 +0300

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,6 @@ export default {
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
 
-    // target
-    target: 'server',
-
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/tailwindcss
